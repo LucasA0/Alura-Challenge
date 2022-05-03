10006f097cc7878c28347377c1957d28f3b6fcb8
Lucas Araujo <104575967+LucasA0@users.noreply.github.com>
2022-05-03 12:20:45 -0300

    Melhorando funcionalidades do Java Script

--- challenge/js/principal.js
+++ challenge/js/principal.js
@@ -11,14 +11,14 @@ document.getElementById('copy').addEventListener('click', function() {/* adicion
 // alterando os caracteres especiais e com acento, assim evitando que a criptografia passe com acentos etc...
 function remover_acentos_espaco(str) {
     /* aqui foi usado da função normalize para remover os acentos das palavras e também foi utilizado
-    de expressões regulares (/[^a-zA-Z\s]/) o que remove todos os caracteres especiais */
+    de expressões regulares (/[^a-zA-Zs]/) o que remove todos os caracteres especiais */
     return str.normalize("NFD").replace(/[^a-zA-Z\s]/g, "");
 }
 
 // pegando do DOM o meu botão de criptografia e adicionando o evento para quando ele for clicado
 var btnAdd = document.getElementById('crypto').addEventListener('click', function(event){
-    // chamando minha função para remover do texto, todos os caracteres indesejados!
-    var input = remover_acentos_espaco(document.getElementById('first').value);
+    // chamando minha função para remover do texto, todos os caracteres indesejados e transformando tudo o que for maiusculo em minusculo .toLowerCase();!
+    var input = remover_acentos_espaco(document.getElementById('first').value.toLowerCase());
     var convert = input.replace(/e/g, 'enter');
     convert = convert.replace(/i/g, 'imes');
     convert = convert.replace(/a/g, 'ai');
@@ -26,7 +26,7 @@ var btnAdd = document.getElementById('crypto').addEventListener('click', functio
     convert = convert.replace(/u/g, 'ufat');
 
     document.getElementById('second').value = convert;// atribuindo o valor do campo de texto primário para o secundário 
-    document.getElementById('first').value = '';// em seguida ele recebe um valo nulo
+    document.getElementById('first').value = '';// em seguida ele recebe um valor nulo
 
 })
 
