99b1772b72115fcd4c652c70fab8ebb908f1cba9
Lucas Araujo <104575967+LucasA0@users.noreply.github.com>
2022-05-03 12:24:49 -0300

    melhorando funcionalidades do Js
    Adição do comando .toLowerCase();

--- challenge/js/principal.js
+++ challenge/js/principal.js
@@ -17,8 +17,8 @@ function remover_acentos_espaco(str) {
 
 // pegando do DOM o meu botão de criptografia e adicionando o evento para quando ele for clicado
 var btnAdd = document.getElementById('crypto').addEventListener('click', function(event){
-    // chamando minha função para remover do texto, todos os caracteres indesejados!
-    var input = remover_acentos_espaco(document.getElementById('first').value);
+    // chamando minha função para remover do texto, todos os caracteres indesejados e transformando tudo o que for maiusculo em minusculo .toLowerCase();!
+    var input = remover_acentos_espaco(document.getElementById('first').value.toLowerCase());
     var convert = input.replace(/e/g, 'enter');
     convert = convert.replace(/i/g, 'imes');
     convert = convert.replace(/a/g, 'ai');
@@ -26,7 +26,7 @@ var btnAdd = document.getElementById('crypto').addEventListener('click', functio
     convert = convert.replace(/u/g, 'ufat');
 
     document.getElementById('second').value = convert;// atribuindo o valor do campo de texto primário para o secundário 
-    document.getElementById('first').value = '';// em seguida ele recebe um valo nulo
+    document.getElementById('first').value = '';// em seguida ele recebe um valor nulo
 
 })
 
